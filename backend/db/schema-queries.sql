50304c4ac726352a404cfc95a9a48f24b57c8bfa
Danny Lieberman <dl@software.co.il>
2026-01-15 18:06:52 +0200

    Migrate Projects to Models, Make basic RBAC infrastructure, ActiveModels table for multi-model support

--- backend/db/schema-queries.sql
+++ backend/db/schema-queries.sql
@@ -1,30 +1,29 @@
 -- FAQ queries on the information schema
 -- Views
+DROP VIEW IF EXISTS public.information_schema_view;
+CREATE OR REPLACE VIEW public.information_schema_view AS
 SELECT
-    '- ' || t.table_name || ' (' || string_agg(c.column_name::text, ', ' ORDER BY c.ordinal_position) || ')' AS view_with_columns
+    t.table_name,t.table_type,t.table_schema,
+    t.table_name || ' (' || string_agg(c.column_name::text, ', ' ORDER BY c.ordinal_position) || ')' AS view_with_columns
 FROM
     information_schema.tables t
 JOIN
     information_schema.columns c ON t.table_name = c.table_name AND t.table_schema = c.table_schema
-WHERE
-    t.table_schema = 'public' -- Replace 'public' with your desired schema name if different
-    AND t.table_type = 'VIEW' -- Filter specifically for views
+
 GROUP BY
-    t.table_name
+    t.table_name, t.table_type
 ORDER BY
     t.table_name;
 
 -- Tables
 SELECT
-    '- ' || t.table_name || ' (' || string_agg(c.column_name::text, ', ' ORDER BY c.ordinal_position) || ')' AS table_with_columns
+    t.table_name,t.table_type,t.table_schema,
+    t.table_name || ' (' || string_agg(c.column_name::text, ', ' ORDER BY c.ordinal_position) || ')' AS table_with_columns
 FROM
     information_schema.tables t
 JOIN
     information_schema.columns c ON t.table_name = c.table_name AND t.table_schema = c.table_schema
-WHERE
-    t.table_schema = 'public' -- Replace 'public' with your desired schema name if different
-    AND t.table_type = 'BASE TABLE' -- Filter specifically for tables
 GROUP BY
-    t.table_name
+    t.table_name, t.table_type
 ORDER BY
     t.table_name;
